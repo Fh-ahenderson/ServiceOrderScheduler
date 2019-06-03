38c9a53fdf25d46f1ed46e2a3fea55cd64451354
Fh-Lydia <lyeager@fischerhomes.com>
2019-06-03 12:14:17 -0400

    Remove unused test case and add shell for exception validation tests

---
+++ ServiceOrderScheduler/DIScheduler.Tests/Repositories/SapphireObject/ValidateSapphireDataShould.cs
@@ -0,0 +1,54 @@
+﻿using DIScheduler.Core.Exceptions;
+using Moq;
+using NUnit.Framework;
+
+namespace DIScheduler.Tests.Repositories.SapphireObject
+{
+    [TestFixture]
+    public class ValidateSapphireDataShould
+    {
+        [Test]
+        public void ThrowMissingLotExceptionGivenLotIsMissing()
+        {
+            // Setup
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+
+
+        [Test]
+        public void ThrowMissingCommunityExceptionGivenCommunityIsMissing()
+        {
+            // Setup
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+
+        [Test]
+        public void ThrowMissingVendorExceptionGivenVendorIsMissing()
+        {
+            // Setup
+
+
+            // Act
+
+
+            // Assert
+
+
+        }
+
+    }
+}
